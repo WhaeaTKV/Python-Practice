357c35750f2dcfa345d198bb7f9406ef269296cf
WhaeaTKV <kapa-viviant@wsc.school.nz>
2025-05-06 09:55:39 +1200

    Milestone_1

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+year = int(input("What year were you born? "))
+current_year = int(input("Enter the current year: "))
+age = current_year - year
+print("You are {} years old in {}.".format(age, current_year))
