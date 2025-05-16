238f7b5528fcc66a6967b3af3f46b83abb240be5
WhaeaTKV <kapa-viviant@wsc.school.nz>
2025-05-16 12:53:20 +1200

    update m2

--- milestone_2/milestone_2_code.py
+++
@@ -1,4 +0,0 @@
-
-#This python programme finds your age based on the year you were born and the current year.
-year = int(input("What year were you born? "))
-add = int(input("Enter the current year: "))
