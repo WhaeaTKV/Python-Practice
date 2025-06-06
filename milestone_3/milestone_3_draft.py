087fd5e058fd341b0ba4816d788f5e20a62557ec
WhaeaTKV <kapa-viviant@wsc.school.nz>
2025-06-06 13:20:06 +1200

    Example_editing

--- milestone_3/milestone_3_draft.py
+++ milestone_3/milestone_3_draft.py
@@ -2,13 +2,17 @@
 print("Welcome to my Quiz about Vampires!")
 print("Test your knowledge and see what you know.")
 
-user_answer = input("What is the name of the German 1930's Vampire cult classic film?")
-A1 = "Nosferatu"
-print("Your answer is: " + user_answer)
+print("Question 1: What is the name of the German 1930's Vampire cult classic film")
+user_answer = input("Is it A: Nosferatu, B: Dracula, or C: Twilight? ")
+A1 = "A" or "Nosferatu"
 if user_answer == A1:
-    print("Correct! Nosferatu is indeed a classic vampire film and is considered one of the greatest")
+    print("Correct!")
 else:
-    print("Incorrect! Better luck next time!")
+    print("Incorrect!")
+    
+print("Better luck next time!")
+
+
 
 # Q2: "When was Dracula first published?"
 # A2:
@@ -25,3 +29,16 @@ else:
 # "Stay safe out there!"
 
 # STOP
+
+
+
+# print("Welcome to my Quiz about Vampires!")
+# print("Test your knowledge and see what you know.")
+
+# user_answer = input("What is the name of the German 1930's Vampire cult classic film?")
+# A1 = "Nosferatu"
+# print("Your answer is: " + user_answer)
+# if user_answer == A1:
+#     print("Correct! Nosferatu is indeed a classic vampire film and is considered one of the greatest")
+# else:
+#     print("Incorrect! Better luck next time!")
